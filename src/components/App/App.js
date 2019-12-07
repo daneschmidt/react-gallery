f107805782bac366c9fe55f7fa4bad7a7af39729
Dane Schmidt <daneschmidt@gmail.com>
2019-12-07 10:43:52 -0600

    images are working on the DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,12 @@ import axios from 'axios';
 
 class App extends Component {
 
-  ////// NEW STUFF
+  ////// NEW STUFF ////////
+
+  state = {
+    pics: []
+
+  }
 
   componentDidMount() {
     this.getPics();
@@ -16,23 +21,37 @@ class App extends Component {
       url: '/gallery'
     })
     .then((response) => {
-        console.log(response);
+        this.setState({
+          pics: response.data
+        }, () => {
+          console.log(this.state.pics);
+        });
     })
     .catch((err) => {
       console.warn(err);
     })
   }
 
-  ////// END NEW STUFF
+  ////// END NEW STUFF /////////
 
   render() {
+
+    const picsArray = this.state.pics.map((item, index) => {
+      return (
+        <div key={index}>
+          <p><img src={item.path}/></p>
+        </div>
+
+      )
+    })
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
         <br/>
-        <p>Gallery goes here</p>
+        {picsArray}
         <img src="images/goat_small.jpg"/>
       </div>
     );
