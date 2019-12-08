f716131e2434bed77d8042da70a686d2f7ca7838
Dane Schmidt <daneschmidt@gmail.com>
2019-12-08 13:53:30 -0600

    conditional rendering is working.  base mode is done

--- src/components/GalleryListItem/GalleryListItem.js
+++ src/components/GalleryListItem/GalleryListItem.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-
+import './Expand.css';
 
 class GalleryListItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show: false,
+        }
+
+    }
+    clickExpand = (event) => {
+        this.setState({
+            show: !this.state.show,
+
+        })
+
+    }
 
     render() {
+        let showClass;
+
+        if (this.state.show) {
+            showClass = "isExpanded"
+        } else if (!this.state.show) {
+            showClass = 'isHidden';
+        }
+
         return (
             <div>
-                <p><img src={this.props.pic.path} /></p>
+                <p className="clickable" onClick={this.clickExpand}><img src={this.props.pic.path} /></p>
+                <div className={showClass}>
                 <p>{this.props.pic.description}</p>
+                </div>
+               
                 <span>
                     <button onClick={() => this.props.likePic(this.props.pic.id)}>LIKE</button>
                     {this.props.pic.likes}
                 </span>
+                
+                
+
             </div>
         )
     }
